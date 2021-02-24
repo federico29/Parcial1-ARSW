d53b02171755565b8e394da2ed193131704e4b53
federico29 <federico.barrios@mail.escuelaing.edu.co>
2021-02-24 08:54:45 -0500

    Punto I y parte del II

--- covidAnalyzerTool/src/main/java/eci/arsw/covidanalyzer/CovidAnalyzerTool.java
+++ covidAnalyzerTool/src/main/java/eci/arsw/covidanalyzer/CovidAnalyzerTool.java
@@ -27,18 +27,19 @@ public class CovidAnalyzerTool {
         resultAnalyzer = new ResultAnalyzer();
         testReader = new TestReader();
         amountOfFilesProcessed = new AtomicInteger();
+        amountOfFilesProcessed.set(0);
     }
 
-    public void processResultData() {
-        amountOfFilesProcessed.set(0);
-        List<File> resultFiles = getResultFileList();
-        amountOfFilesTotal = resultFiles.size();
-        for (File resultFile : resultFiles) {
+    public void processResultData(List<File> filesDivision) {
+        //List<File> resultFiles = getResultFileList();
+        //amountOfFilesTotal = filesDivision.size();
+        for (File resultFile : filesDivision) {
             List<Result> results = testReader.readResultsFromFile(resultFile);
             for (Result result : results) {
                 resultAnalyzer.addResult(result);
             }
             amountOfFilesProcessed.incrementAndGet();
+            System.out.println(amountOfFilesProcessed.get());
         }
     }
 
@@ -52,23 +53,86 @@ public class CovidAnalyzerTool {
         return csvFiles;
     }
 
-
     public Set<Result> getPositivePeople() {
         return resultAnalyzer.listOfPositivePeople();
+    }
+
+    public AtomicInteger getAmountOfFilesProcessed(){
+        return amountOfFilesProcessed;
+    }
+
+    private List<File> divideFiles(List<File> resultFileList, int min, int max) {
+        return resultFileList.subList(min, max);
+    }
+
+    public static void pararHilos(List<CovidThread> hilos){
+        for (CovidThread hilo: hilos){
+            hilo.suspenderHilo();
+        }
+    }
+
+    /**
+     * Método que reanuda la ejecución de todos los hilos que se tienen
+     * @param hilos Lista con todos los hilos creados para resolver el problema
+     */
+    public static void reanudarHilos(List<CovidThread> hilos){
+        for (CovidThread hilo: hilos){
+            hilo.renaudarHilo();
+        }
     }
 
     /**
      * A main() so we can easily run these routing rules in our IDE
      */
-    public static void main(String... args) throws Exception {
+    public static void main(String[] args) throws Exception {
         CovidAnalyzerTool covidAnalyzerTool = new CovidAnalyzerTool();
-        Thread processingThread = new Thread(() -> covidAnalyzerTool.processResultData());
-        processingThread.start();
+        Integer numberOfThreads = 5;
+        List<File> resultFileList = covidAnalyzerTool.getResultFileList();
+        List<CovidThread> threads = new ArrayList<>();
+        AtomicInteger count = covidAnalyzerTool.getAmountOfFilesProcessed();
+        Integer cont = 0;
+
+        System.out.println(resultFileList.size());
+        System.out.println(resultFileList.size() % numberOfThreads != 0);
+
+        if (resultFileList.size() % numberOfThreads != 0) {
+            for (int i = 0; i < numberOfThreads - 1; i++) {
+                List<File> filesDivision = covidAnalyzerTool.divideFiles(resultFileList, cont, cont + numberOfThreads);
+                System.out.println(cont + "---" + (cont + numberOfThreads - 1));
+                threads.add(new CovidThread(filesDivision, count));
+                cont = cont + numberOfThreads;
+            }
+            System.out.println(cont + "---" + (resultFileList.size() - 1));
+            List<File> filesDivision = covidAnalyzerTool.divideFiles(resultFileList, cont, resultFileList.size());
+            threads.add(new CovidThread(filesDivision, count));
+        } else {
+            for (int i = 0; i < numberOfThreads; i++) {
+                List<File> filesDivision = covidAnalyzerTool.divideFiles(resultFileList, cont, cont + numberOfThreads);
+                threads.add(new CovidThread(filesDivision, count));
+                cont = cont + numberOfThreads;
+            }
+        }
+
+        threads.forEach(thread -> {
+            thread.start();
+        });
+
+        threads.forEach(thread -> {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
         while (true) {
             Scanner scanner = new Scanner(System.in);
             String line = scanner.nextLine();
             if (line.contains("exit"))
                 break;
+            threads.forEach(thread -> {
+                thread.suspenderHilo();
+            });
             String message = "Processed %d out of %d files.\nFound %d positive people:\n%s";
             Set<Result> positivePeople = covidAnalyzerTool.getPositivePeople();
             String affectedPeople = positivePeople.stream().map(Result::toString).reduce("", (s1, s2) -> s1 + "\n" + s2);
---
+++ covidAnalyzerTool/src/main/java/eci/arsw/covidanalyzer/CovidThread.java
@@ -0,0 +1,53 @@
+package eci.arsw.covidanalyzer;
+
+import java.io.File;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CovidThread extends Thread{
+    private List<File> files;
+    private TestReader testReader;
+    private ResultAnalyzer resultAnalyzer;
+    private boolean suspender;
+    private AtomicInteger amountOfFilesProcessed;
+
+
+
+    public CovidThread(List<File> filesDivision, AtomicInteger count) {
+        this.files = filesDivision;
+        testReader = new TestReader();
+        resultAnalyzer = new ResultAnalyzer();
+        suspender = false;
+        amountOfFilesProcessed = count;
+    }
+
+    @Override
+    public void run() {
+        synchronized (this) {
+            while (suspender) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            for (File resultFile : this.files) {
+                List<Result> results = testReader.readResultsFromFile(resultFile);
+                System.out.println("HH");
+                for (Result result : results) {
+                    resultAnalyzer.addResult(result);
+                }
+                amountOfFilesProcessed.getAndIncrement();
+            }
+        }
+    }
+
+    synchronized void suspenderHilo(){
+        suspender=true;
+    }
+
+    synchronized void renaudarHilo(){
+        suspender=false;
+        notify();
+    }
+}
